893fb7040ce139c0269dc19a0ab2941fc2b65848
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2024-09-23 12:45:00 -0700

    signal-02 project

--- SystemProgramming/LinuxUnix/06-Signals/signal-02/main.cpp
+++ SystemProgramming/LinuxUnix/06-Signals/signal-02/main.cpp
@@ -1,34 +1,38 @@
 #include <iostream>
 #include <signal.h>
+#include <unistd.h>
 #include <thread>
 #include <chrono>
 
 using std::cout, std::endl, std::cerr;
 using std::this_thread::sleep_for, std::chrono::seconds;
 
-static void handler(int signal)
+static void signal_handler(int sig)
 {
     static size_t count{0};
 
-    if (signal == SIGINT) {
+    if (sig == SIGINT) {
         cout << "\nPressed Ctrl+C " << (++count) << '\n';
         return; 
     }
-
     cout << "Ctrl+\\ (SIGQUIT) pressed! Bye!\n";
     _exit(EXIT_SUCCESS);
 }
 
+void install_signal_handler(int sig)
+{
+    if (signal(sig, signal_handler) == SIG_ERR) {
+        cerr << "Error to install the signal handler for " << sig << '\n';
+        abort();
+    }
+}
+
 int main()
 {
-    if (signal(SIGINT, handler) == SIG_ERR) {
-        cerr << "Error to install the signal handler for SIGINT\n";
-        abort();
-    }
-    if (signal(SIGQUIT, handler) == SIG_ERR) {
-        cerr << "Error to install the signal handler for SIGQUIT\n";
-        abort();
-    }
+    cout << "PID: " << getpid() << '\n';
+
+    install_signal_handler(SIGINT);
+    install_signal_handler(SIGQUIT);
 
     for (int i{0}; ; ++i) {
         cout << "Waiting " << i << '\n';

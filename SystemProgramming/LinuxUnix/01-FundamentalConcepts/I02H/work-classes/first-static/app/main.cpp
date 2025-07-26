7ba3dacb04ad6e5f1ad38a08efee2c2f62324740
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-07-26 19:16:44 +0200

    work-classes/first-static

--- SystemProgramming/LinuxUnix/01-FundamentalConcepts/I02H/work-classes/first-static/app/main.cpp
+++ SystemProgramming/LinuxUnix/01-FundamentalConcepts/I02H/work-classes/first-static/app/main.cpp
@@ -5,12 +5,14 @@
 
 int main()
 {
-	std::vector a{1,2,3,4,5};
-	std::vector b{1,2,3,4,5};
-
+	std::vector<int> a{1,2,3,4,5};
+	std::vector<int> b{2,3,4,5,6};
+	
 	hypernex::add_vector(a, b);
 
 	std::copy(a.begin(), a.end(), std::ostream_iterator<int>{std::cout, ", "});
 
 	return EXIT_SUCCESS;
 }
+
+

e9ddb8170be17f438b7445016fad20c03c1d0c8c
Eduardo Ribeiro da Silva <filosofisto@hotmail.com>
2025-07-27 12:29:23 +0200

    Transform to shared library

--- SystemProgramming/LinuxUnix/01-FundamentalConcepts/I02H/work-classes/first-shared/build.sh
+++ SystemProgramming/LinuxUnix/01-FundamentalConcepts/I02H/work-classes/first-shared/build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 echo "Compiling library artifacts..."
-g++ -c math/add_vector.cpp -o build/add_vector.o
-g++ -c math/sub_vector.cpp -o build/sub_vector.o
+g++ -fPIC -c math/add_vector.cpp -o build/add_vector.o
+g++ -fPIC -c math/sub_vector.cpp -o build/sub_vector.o
 
-echo "Create the static library file (.a)..."
-ar rs build/libhypernex_math.a build/add_vector.o build/sub_vector.o
+echo "Create the shared library file (.so)..."
+g++ -shared -o build/libhypernex_math.so build/add_vector.o build/sub_vector.o
 
 echo "Build App..."
-g++ app/main.cpp -Imath -Lbuild -lhypernex_math -o build/app
+g++ app/main.cpp -Imath -Lbuild -lhypernex_math -o build/app -Wl,-rpath=./
 
 echo "Done!"
 
